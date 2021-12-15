a563f0718caf8ef16937104954aaf49951ba3ed7
ffakira <55924290+ffakira@users.noreply.github.com>
2021-12-15 12:27:10 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const withTM = require('next-transpile-modules')([
+  '@blocto/sdk',
+  '@project-serum/sol-wallet-adapter',
+  '@solana/wallet-adapter-base',
+  '@solana/wallet-adapter-react',
+  '@solana/wallet-adapter-react-ui',
+  '@solana/wallet-adapter-ledger',
+  '@solana/wallet-adapter-phantom',
+  '@solana/wallet-adapter-slope',
+]);
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withTM({
   reactStrictMode: true,
-}
+  webpack5: true,
+});
