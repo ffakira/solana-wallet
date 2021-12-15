3438958199b946976d7888b0babfbecdaf041d5c
ffakira <55924290+ffakira@users.noreply.github.com>
2021-12-15 12:56:24 +0800

    feat: added tailwind css

---
+++ components/Nav.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
+
+export const Nav = (): JSX.Element => {
+  return(
+    <nav>
+      <WalletMultiButton />
+    </nav>
+  )
+}
+
+export default Nav
