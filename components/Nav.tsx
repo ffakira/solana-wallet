a0ddf3d7fc1e43ff9e1c94537c84ad0fe8204662
ffakira <55924290+ffakira@users.noreply.github.com>
2021-12-15 13:01:04 +0800

    fix: eslint issues

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
 
 export const Nav = (): JSX.Element => {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,29 @@
-import '../styles/globals.css';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
-import { FC, ReactNode } from 'react';
+import "../styles/globals.css"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
+import { AppProps } from "next/app"
+import dynamic from "next/dynamic"
+import { FC, ReactNode } from "react"
 
-require('@solana/wallet-adapter-react-ui/styles.css');
+require("@solana/wallet-adapter-react-ui/styles.css")
 
 const WalletConnectionProvider = dynamic<{ children: ReactNode }>(
-    () =>
-        import('../components/WalletConnectionProvider').then(
-            ({ WalletConnectionProvider }) => WalletConnectionProvider
-        ),
-    {
-        ssr: false,
-    }
-);
+  () =>
+    import("../components/WalletConnectionProvider").then(
+      ({ WalletConnectionProvider }) => WalletConnectionProvider
+    ),
+  {
+    ssr: false,
+  }
+)
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-    return (
-        <WalletConnectionProvider>
-            <WalletModalProvider>
-                <Component {...pageProps} />
-            </WalletModalProvider>
-        </WalletConnectionProvider>
-    );
-};
+  return (
+    <WalletConnectionProvider>
+      <WalletModalProvider>
+        <Component {...pageProps} />
+      </WalletModalProvider>
+    </WalletConnectionProvider>
+  )
+}
 
-export default App;
+export default App
